e72fda9ab65cc334d1a1a99f07c1c59dadeb7799
Christopher Horner <horner.cd@gmail.com>
2014-07-27 11:21:26 +1000

    Initial commit.

---
+++ rhythmsticks-example/src/main/java/com/example/rhythmsticks/ExampleActivity.java
@@ -0,0 +1,15 @@
+package com.example.rhythmsticks;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Christopher Horner
+ */
+public class ExampleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
---
+++ rhythmsticks/src/main/java/com/chrishorner/rhythmsticks/RhythmFrameLayout.java
@@ -0,0 +1,10 @@
+package com.chrishorner.rhythmsticks;
+
+import android.content.Context;
+import android.widget.FrameLayout;
+
+public class RhythmFrameLayout extends FrameLayout {
+    public RhythmFrameLayout(Context context) {
+        super(context);
+    }
+}
